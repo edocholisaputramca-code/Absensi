62dc3afcc5f63e8baabcada0e4b0692a88eebe9f
edocholisaputramca-code <edocholisaputramca@gmail.com>
2026-09-14 10:27:53 +0700

    Delete demo-absensi directory

--- demo-absensi/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Menyusun ulang index.html & dev.html dari folder src/
-cd "$(dirname "$0")" || exit 1
-node build.js
--- demo-absensi/src/17-init.js
+++
@@ -1,5 +0,0 @@
-/* ---------------- init ---------------- */
-(function init(){
-  document.getElementById('roleSel').innerHTML = Object.keys(ROLES).map(k => '<option value="' + k + '">' + ROLES[k].nama + '</option>').join('');
-  clockTick(); render();
-})();
